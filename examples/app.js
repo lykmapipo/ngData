c50002251f3a738b217e78e9715eb1b4cf006e56
Benson Maruchu <bengenius12@gmail.com>
2015-09-10 16:42:11 +0300

    add all the dependencies of ngData to index.html

--- examples/app.js
+++ examples/app.js
@@ -5,7 +5,11 @@
   // example dir for testing ngData module
   angular
         .module('webexample',['ngData'])
-        .config(function(){
-
+        .config(function($databaseProvider){
+          
+            $databaseProvider.name = 'testDB';
+            $databaseProvider.description = 'this is a test db';
+            $databaseProvider.version = '1.0.0';
+            $databaseProvider.size = 8 *1024 *1024;
         });
 }());
