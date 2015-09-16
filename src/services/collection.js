94b0015ca592faf56ba88f79b8a12d2ddcf6e700
Benson Maruchu <bengenius12@gmail.com>
2015-09-16 15:51:31 +0300

    update the collection documentation

--- src/services/collection.js
+++ src/services/collection.js
@@ -10,7 +10,7 @@
 
             /**
              * @description model
-             * @param {[type]} table [description]
+             * @param {type} table [description]
              */
              function Collection(options){
                this.table = options.tablename;
@@ -19,9 +19,9 @@
             /**
              * @description Shortcut for creating a new Document that is
              *              automatically saved to the db if valid.
-             * @param {[Object,Array]} doc
-             * @param {[Function]} callback
-             * @return {[type]} [description]
+             * @param {(Object|Array)} doc
+             * @param {Function} callback
+             * @return {type}
              */
             Collection.prototype.create = function (/*doc, callback*/) {
 
@@ -29,9 +29,9 @@
 
             /**
              * @description Updates documents in the database without returning them.
-             * @param  {[Object]}   conditions
-             * @param  {[Object]}   doc
-             * @param  {[Object]}   options
+             * @param  {Object}   conditions
+             * @param  {Object}   doc
+             * @param  {Object}   options
              * @param  {Function} callback
              */
             Collection.prototype.update = function (/*conditions, doc, options, callback*/) {
@@ -40,9 +40,9 @@
 
             /**
              * @description Removes documents from the collection.
-             * @param  {[Object]}   conditions
+             * @param  {Object}   conditions
              * @param  {Function} callback
-             * @return {[type]}
+             * @return {type}
              */
             Collection.prototype.delete = function (/*conditions, callback*/) {
 
@@ -50,11 +50,11 @@
 
             /**
              * @description find documents
-             * @param  {[Object]}   conditions
-             * @param  {[Object]}   projections [optional fields to return]
-             * @param  {[Object]}   options     [optional]
+             * @param  {Object}   conditions
+             * @param  {Object}   projections [optional fields to return]
+             * @param  {Object}   options     [optional]
              * @param  {Function} callback
-             * @return {[Qurey]}
+             * @return {Qurey}
              */
             Collection.prototype.find = function (/*conditions, projections, options, callback*/) {
 
@@ -63,11 +63,11 @@
             /**
              * @description Finds a single document by its _id field.
              *              findById(id) is equivalent to findOne({ id: id }).
-             * @param  {[Object, String, Number]}   id     [value of `id` to query by]
-             * @param  {[Object]}   projections [optional fields to return]
-             * @param  {[Object]}   options     [optional]
+             * @param  {(Object|String|Number)}   id     value of `id` to query by
+             * @param  {Object}   projections optional fields to return
+             * @param  {Object}   options     [optional]
              * @param  {Function} callback
-             * @return {[Query]}               [description]
+             * @return {Query}               [description]
              */
             Collection.prototype.findById = function (/*id, projections, options, callback*/) {
 
@@ -77,22 +77,22 @@
              * @description Issue a mongodb findAndModify remove command by a
              *               document's _id field. findByIdAndRemove(id, ...) is
              *               equivalent to findOneAndRemove({ id: id }, ...).
-             * @param  {[Object, String, Number]}   id       [value of `id` to query by]
-             * @param  {[Object]}   options
+             * @param  {(Object|String|Number)}   id      value of `id` to query by
+             * @param  {Object}   options
              * @param  {Function} callback
-             * @return {[type]}            [description]
+             * @return {type}            [description]
              */
             Collection.prototype.findByIdAndDelete = function (/*id, options, callback*/) {
               // body...
             };
 
             /**
-             * @description [Issues a mongodb findAndModify update command by a
+             * @description Issues a mongodb findAndModify update command by a
              *              document's id field. findByIdAndUpdate(id, ...) is
-             *              equivalent to findOneAndUpdate({ id: id }, ...).]
-             * @param  {[Object]}   id       [value of `id` to query by]
-             * @param  {[Object]}   update
-             * @param  {[Object]}   options
+             *              equivalent to findOneAndUpdate({ id: id }, ...).
+             * @param  {Object}   id       value of `id` to query by
+             * @param  {Object}   update
+             * @param  {Object}   options
              * @param  {Function} callback
              * @return {[type]}            [description]
              */
@@ -101,21 +101,21 @@
             };
 
             /**
-             * @description [Find One document]
-             * @param  {[Object]}   conditions
-             * @param  {[Object]}   projections [ optional fields to return ]
-             * @param  {[Object]}   options
+             * @description Find One document
+             * @param  {Object}   conditions
+             * @param  {Object}   projections [ optional fields to return ]
+             * @param  {Object}   options
              * @param  {Function} callback
-             * @return {[Query]}               [this]
+             * @return {Query}               [this]
              */
             Collection.prototype.findOne = function (/*conditions, projections, options, callback*/) {
 
             };
 
             /**
-             * @description [Issue a findAndModify remove command]
-             * @param  {[Object]}   conditions [description]
-             * @param  {[Object]}   options    [description]
+             * @description Issue a findAndModify remove command
+             * @param  {Object}   conditions [description]
+             * @param  {Object}   options    [description]
              * @param  {Function} callback   [description]
              * @return {[type]}              [description]
              */
@@ -125,11 +125,11 @@
 
             /**
              * @description Issues a mongodb findAndModify update command.
-             * @param  {[type]}   conditions [description]
-             * @param  {[type]}   update     [description]
-             * @param  {[type]}   options    [description]
-             * @param  {Function} callback   [description]
-             * @return {[Query]}              [description]
+             * @param  {Object}   conditions
+             * @param  {Object}   update
+             * @param  {Object}   options
+             * @param  {Function} callback
+             * @return {Query}
              */
             Collection.prototype.findOneAndUpdate = function(/*conditions, update, options, callback*/) {
 
@@ -138,9 +138,9 @@
             /**
              * @description Creates a Query, applies the passed conditions, and
              *              returns the Query.
-             * @param  {[String]} path [description]
-             * @param  {[Object]} val  [optional]
-             * @return {[Query]}      [description]
+             * @param  {String} path [description]
+             * @param  {Object} val  [optional]
+             * @return {Query}      [description]
              */
             Collection.prototype.where = function (/*path, val*/) {
 
@@ -149,8 +149,8 @@
             /**
              * @description Specifies the maximum number of records the query
              *              will return. can not be used with distinct
-             * @param  {[Number]} val [description]
-             * @return {[type]}     [description]
+             * @param  {Number} val [description]
+             * @return {type}     [description]
              */
             Collection.prototype.limit = function (/*val*/) {
 
@@ -158,8 +158,8 @@
 
             /**
              * @description Sets the sort order
-             * @param  {[Object, String]} arg [description]
-             * @return {[Query]}    [this]
+             * @param  {(Object|String)} arg [description]
+             * @return {Query}    [this]
              */
             Collection.prototype.sort = function (/*arg*/) {
 
@@ -167,8 +167,8 @@
 
             /**
              * @description Specifies a $gt query condition.
-             * @param  {[String]} path
-             * @param  {[Number]} val
+             * @param  {String} path
+             * @param  {Number} val
              * @return {[type]}      [description]
              */
             Collection.prototype.gt = function (/*path, val*/) {
@@ -177,8 +177,8 @@
 
             /**
              * @description Specifies a $gte query condition.
-             * @param  {[String]} path
-             * @param  {[Number]} val
+             * @param  {String} path
+             * @param  {Number} val
              * @return {[type]}      [description]
              */
             Collection.prototype.gte = function (/*path, val*/) {
@@ -187,8 +187,8 @@
 
             /**
              * @description Specifies a $lt query condition.
-             * @param  {[String]} path
-             * @param  {[Number]} val
+             * @param  {String} path
+             * @param  {Number} val
              * @return {[type]}      [description]
              */
             Collection.prototype.lt = function (/*path, val*/) {
@@ -197,8 +197,8 @@
 
             /**
              * @description Specifies a $lte query condition.
-             * @param  {[String]} path
-             * @param  {[Number]} val
+             * @param  {String} path
+             * @param  {Number} val
              * @return {[type]}      [description]
              */
             Collection.prototype.lte = function (/*path, val*/) {
@@ -207,8 +207,8 @@
 
             /**
              * @description Specifies a $in query condition.
-             * @param  {[String]} path
-             * @param  {[Number]} val
+             * @param  {String} path
+             * @param  {Number} val
              * @return {[type]}      [description]
              */
             Collection.prototype.in = function (/*path, val*/) {
@@ -218,8 +218,8 @@
             /**
              * @description Specifies which document fields to include or
              *              exclude (also known as the query "projection")
-             * @param  {[Object, String]} arg
-             * @return {[Query]}     [this]
+             * @param  {(Object|String)} arg
+             * @return {Query}     [this]
              */
             Collection.prototype.select = function (/*arg*/) {
 
@@ -227,8 +227,8 @@
 
             /**
              * @description Specifies arguments for an $and condition.
-             * @param  {[Array]} options
-             * @return {[Query]}         [this]
+             * @param  {Array} options
+             * @return {Query}         [this]
              */
             Collection.prototype.and = function (/*options*/) {
 
@@ -236,8 +236,8 @@
 
             /**
              * @description Specifies arguments for an $or condition.
-             * @param  {[Array]} array
-             * @return {[Query]}       [this]
+             * @param  {Array} array
+             * @return {Query}       [this]
              */
             Collection.prototype.or = function (/*array*/) {
 
@@ -245,8 +245,8 @@
 
             /**
              * @description Specifies arguments for an $nor condition.
-             * @param  {[Array]} array
-             * @return {[Query]}       [this]
+             * @param  {Array} array
+             * @return {Query}       [this]
              */
             Collection.prototype.nor  = function (/*array*/) {
 
@@ -254,8 +254,8 @@
 
             /**
              * @description Defines an index (most likely compound) for this schema.
-             * @param  {[String]} fields  [description]
-             * @param  {[type]} options [description]
+             * @param  {String} fields
+             * @param  {Object} options
              * @return {[type]}         [description]
              */
              Collection.prototype.index = function (/*fields, options*/) {
@@ -264,9 +264,9 @@
 
             /**
              * @description Specifying this query as a count query.
-             * @param  {[Object]}   criteria
-             * @param  {function} callback
-             * @return {[query]} this
+             * @param  {Object}   criteria
+             * @param  {Function} callback
+             * @return {Query} this
              */
             Collection.prototype.count = function (/*criteria, callback*/) {
 
@@ -274,8 +274,8 @@
 
             /**
              * @description Declares or executes a distinct() operation.
-             * @param  {[String]}   fields
-             * @param  {[Object, Query]}   criteria
+             * @param  {String}   fields
+             * @param  {(Object|Query)}   criteria
              * @param  {Function} callback
              * @return {[type]}
              */
@@ -286,7 +286,7 @@
             /**
              * @description Specifies the complementary comparison value for
              *               paths specified with where()
-             * @param  {[Object]} val [description]
+             * @param  {Object} val
              * @return {[type]}     [description]
              */
             Collection.prototype.equals = function (/*val*/) {
@@ -295,8 +295,8 @@
 
             /**
              * @description Specifies an $exists condition
-             * @param  {[String]} path [description]
-             * @param  {[Number]} val  [description]
+             * @param  {String} path
+             * @param  {Number} val
              * @return {[type]}      [description]
              */
             Collection.prototype.exists = function (/*path, val*/) {
@@ -305,7 +305,7 @@
 
             /**
              * @description Executes this query and returns a promise
-             * @return {[promise]}
+             * @return {promise}
              */
             Collection.prototype.then = function () {
               // body...
