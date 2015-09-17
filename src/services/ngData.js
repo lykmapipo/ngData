0d50932dd78572f11c220780e9609be33e9fa0d9
lykmapipo <lallyelias87@gmail.com>
2015-09-17 14:58:36 +0300

    initializing  factory

---
+++ src/services/ngData.js
@@ -0,0 +1,47 @@
+(function() {
+    'use strict';
+
+    /**
+     * @ngdoc module
+     * @name ngData.$ngData
+     * @description exports object of the `$ngData` which will be used across an
+     *              app
+     *
+     * @example <caption>registering a new model</caption>
+     * angular
+     * 	.module('<moduleName>')
+     * 	.factory('<factoryName>', function($ngData){
+     * 		//create $ngData model
+     * 		var Customer = $ngData.model('User',{
+     * 				tableName:'customers',
+     * 				timestamp:true,
+     * 				properties:{
+     * 					name:{
+     * 						type:String,
+     * 						required:true
+     * 					},
+     * 					code:String
+     * 					email:{
+     * 						type:String,
+     * 						email:true,
+     * 						required:true
+     * 					},
+     * 					joinedAt:{
+     * 						type:Date,
+     * 						defaultsTo: new Date()
+     * 					}
+     * 				}
+     * 			});
+     * 			
+     * 		//return created model
+     * 		return Customer;
+     * 	});
+     * 
+     * @public
+     */
+    angular
+        .module('ngData')
+        .factory('$ngData', function() {
+            // body...
+        });
+}());
